0a0002e89ca51badc257ec33504697e1cb402858
aengus <aengusmccullough@hotmail.com>
2020-07-07 21:38:48 -0700

    SKAPI-51 SKAPI-52 tidy

--- src/graphql/search/retrieve_rockset_api_key.js
+++ src/graphql/search/retrieve_rockset_api_key.js
@@ -1,31 +1,31 @@
 
 exports.handler = async (event, context, callback) => {
 
-    const AWS = require('aws-sdk')
+    const AWS = require("aws-sdk");
 
     AWS.config.update({
         region: process.env.region
-    })
+    });
 
-    const parameterStore = new AWS.SSM()
+    const parameterStore = new AWS.SSM();
 
-    const getParam = param => {
+    const getParam = (param) => {
         return new Promise((res, rej) => {
             parameterStore.getParameter({
                 Name: param
             }, (err, data) => {
                 if (err) {
-                    return rej(err)
+                    return rej(err);
                 }
-                return res(data)
+                return res(data);
             })
         })
-    }
+    };
     const stage = process.env.stage;
-    const param = await getParam(stage+'-rockset-api-key');
+    const param = await getParam(stage+"-rockset-api-key");
 
-    console.log('ssm = ' + JSON.stringify(param));
-    console.log('context: ', context);
-    console.log('VTL details: ', event);
+    console.log("ssm = " + JSON.stringify(param));
+    console.log("context: ", context);
+    console.log("VTL details: ", event);
     callback(null, param);
 };
