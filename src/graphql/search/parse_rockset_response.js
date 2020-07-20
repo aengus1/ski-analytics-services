8d3f77aefc0cda716492627151787947b21d888c
aengus <aengusmccullough@hotmail.com>
2020-07-20 11:01:13 -0700

    SKAPI-56  tidy

--- src/graphql/search/parse_rockset_response.js
+++ src/graphql/search/parse_rockset_response.js
@@ -1,10 +1,4 @@
 exports.handler = async (event, context, callback) => {
 
-    console.log("context: ", context);
-    console.log("VTL details: ", event);
-
-    event[0].results.forEach(x => {
-        console.log(JSON.stringify(x));
-    });
     return event[0].results;
 };
